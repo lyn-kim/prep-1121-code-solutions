c3da1b2b57d21198eff7236cf27d47170314bf3b
Lyn Kim <lynkim216@outlook.com>
2021-11-17 10:45:22 -0800

    completed convertHoursToMinutes and getGreeting functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -2,5 +2,19 @@ function addTwoNumbers(num1, num2) {
   return num1 + num2;
 }
 
-var addTwoNumbersResult = addTwoNumbers(4, 4);
-console.log('addTwoNumbers Exercise: ', addTwoNumbersResult);
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+var getGreetingResult = getGreeting('World');
+console.log('getGreeting Exercise:', getGreetingResult);
